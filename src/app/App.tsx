38ea8ba30e603e93607e2840d800599976856ee9
NistonN <nisprogrammer@gmail.com>
2025-07-02 22:14:34 +0300

    video player

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,45 +1,49 @@
+import { VideoPlayer } from "@/components/VideoPlayer";
 import { animeTitleService } from "@/services/animeTitleService";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Swiper, SwiperSlide } from "swiper/react";
-import animePoster from "/image/animePoster.jpg";
 
 export const App = () => {
   const { data } = useQuery({
     queryKey: ["get_anime_updates"],
     queryFn: () =>
       animeTitleService.getTitleUpdates({
-        filter: "id,names,description,posters,status",
+        filter: "id,names,genres,description,posters,status,player",
         limit: 5,
       }),
   });
 
   useEffect(() => {
     console.log(`${import.meta.env.VITE_URL}storage/releases/posters/9000/NBPPaSwgJrcoO4eg__f003bb6841ce26560a643491c197878f.jpg`);
-  }, []);
+    console.log(data);
+  }, [data]);
 
   return (
     <>
       <div>
         <Swiper spaceBetween={0} slidesPerView={1}>
-          {data?.list && data.list.length > 0 ? (
-            data.list.map((elem) => (
-              <SwiperSlide key={elem.id}>
-                <div>
-                  <img src={`${import.meta.env.VITE_URL}${elem.posters.medium.url}`} alt="poster" />
+          {data?.list.map((elem) => {
+            return (
+              <>
+                <SwiperSlide key={elem.id}>
                   <div>
-                    <p></p>
+                    <div>
+                      <img src={`${import.meta.env.VITE_URL}${elem.posters.medium.url}`} alt="poster" />
+                    </div>
+                    <div>
+                      <VideoPlayer video={elem} />
+                    </div>
+                    <div>
+                      <p></p>
+                    </div>
                   </div>
-                </div>
-              </SwiperSlide>
-            ))
-          ) : (
-            <SwiperSlide>
-              <img src={animePoster} alt="default poster" />
-            </SwiperSlide>
-          )}
+                </SwiperSlide>
+              </>
+            );
+          })}
         </Swiper>
       </div>
     </>
