178f1dd777c76279814214468c27f94a68a36ac1
NistonN <nisprogrammer@gmail.com>
2025-07-06 23:51:33 +0300

    fix

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -1,6 +1,6 @@
 import type { ITitle } from "@/types/title.type";
 import { MediaPlayer, MediaPlayerInstance, MediaProvider, useMediaStore } from "@vidstack/react";
-import { useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 type Props = {
   video: ITitle;
@@ -9,9 +9,10 @@ type Props = {
 export const VideoPlayer = ({ video }: Props) => {
   const [videoHost] = useState(video.player.host);
   const player = useRef<MediaPlayerInstance>(null);
+  const [listPlayer, setListPlayer] = useState(Object.values(video.player?.list || {}));
 
   const [isPlaying, setPlaying] = useState<boolean>(false);
-  const { fullscreen } = useMediaStore(player);
+  const { fullscreen, qualities, quality, autoQuality, canSetQuality } = useMediaStore(player);
 
   const toggleFullscreen = () => {
     if (fullscreen) {
@@ -43,6 +44,10 @@ export const VideoPlayer = ({ video }: Props) => {
         "",
         "#EXT-X-STREAM-INF:RESOLUTION=1280x720",
         video.player.list?.[1]?.hls?.hd ? `https://${videoHost}${video.player.list[1].hls.hd}` : "",
+        "",
+        "#EXT-X-STREAM-INF:RESOLUTION=1920x1080",
+        video.player.list?.[1]?.hls?.fhd ? `https://${videoHost}${video.player.list[1].hls.fhd}` : "",
+        // video.player.list.filter((quality) => quality.hls.fhd != ""),
       ].join("\n"),
     [videoHost, video.player.list],
   );
@@ -54,12 +59,33 @@ export const VideoPlayer = ({ video }: Props) => {
     return URL.createObjectURL(blob);
   }, [playlist]);
 
+  /*
+    qualities Доступные варианты качества видео
+    quality Текущий вариант качества видео
+    autoQuality Автоматический выбор качества
+    canSetQuality Возможность ручного выбора качества
+  */
+
+  useEffect(() => {
+    console.log(autoQuality, quality, player.current?.qualities);
+  }, [qualities, quality, autoQuality, canSetQuality]);
+
+  // useEffect(() => {
+  //   if (!video.player) {
+  //     return;
+  //   } else {
+  //     listPlayer.map((elem) => {
+  //       console.log(elem.hls.sd);
+  //     });
+  //   }
+  // }, [video]);
+
   if (!video.player) {
     return <div>Видео недоступно</div>;
   }
 
   return (
-    <div className="video-player">
+    <div className="w-full h-screen">
       <div>Видео код: {video.code}</div>
 
       <MediaPlayer
